88322e1777afb5a5b7043636ae998d31364fc35c
Daan Verbeek <daanverbeek13@gmail.com>
2024-03-19 14:59:50 +0100

    Added color picker

--- src/components/detail/ColorPicker.tsx
+++ src/components/detail/ColorPicker.tsx
@@ -1,14 +1,25 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const ColorPicker: FC = () => {
-  const currentColor = useState<string>('#000');
+  const [currentColor, setColor] = useState<string>('#000');
+
+  useEffect(() => {
+    console.log('Color changed to:', currentColor)
+    document.getElementById('colorRound')?.style.setProperty('background-color', currentColor);
+  }, [currentColor]);
   return (
-    <div className='rounded-full p-5 overflow-hidden flex items-center justify-center'>
+    <div className='rounded-full relative  p-5 overflow-hidden flex items-center justify-center flex-col'>
+      <div id='colorRound' className={`w-80 h-80  rounded-full bg-${currentColor}`}></div>
       <input
         type='color'
-        className='h-80 w-80 rounded-full'
+        value={currentColor}
+        className='h-full w-full opacity-0 absolute'
+        onChange={(event) => {
+          setColor(event.target.value);
+        }}
       />
     </div>
+
   );
 }
 
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,17 +1,27 @@
 import { NextPage } from "next";
 import Header from "@/components/shared/Header";
 import ColorPicker from "@/components/detail/ColorPicker";
+import { Power } from "@/components/icons/Power";
+import { useState } from "react";
 
 const DetailPage: NextPage = () => {
+  const [isOn, toggle] = useState<boolean>(false);
   return <>
     <Header backButton={true}/>
 
     <div className='flex flex-row container m-auto w-full'>
-      <div className='w-full '>h</div>
+      <div className='w-full '></div>
       <div className='w-full '>
         <ColorPicker/>
       </div>
       <div className='w-full '></div>
+    </div>
+
+    <div className='w-full flex items-center justify-center mt-24'>
+      <Power
+        clickHandler={() => toggle((prev) => !prev)}
+        className={`w-24 h-24 cursor-pointer transition-colors duration-300 ${isOn ? 'text-blue-500' : 'text-black'}`}
+      />
     </div>
   </>
 }
